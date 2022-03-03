7a3cbd03e916020d5aab08602b647ba62a3b2339
qazqaz1000 <qazqaz1000@hanmail.net>
2022-03-03 21:58:30 +0900

    Readme 수정

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MVVM + AAC  Study
+## 간단한 연락처 앱으로 AAC아키텍쳐 공부
+### 사용된 기술
+#### Room, ViewModel, DataBinding
--- app/src/main/java/com/nckim/mvvmaacstudy/views/main/ContactAdapter.kt
+++ app/src/main/java/com/nckim/mvvmaacstudy/views/main/ContactAdapter.kt
@@ -3,8 +3,10 @@ package com.nckim.mvvmaacstudy.views.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.nckim.mvvmaacstudy.R
+import com.nckim.mvvmaacstudy.databinding.ContactItemBinding
 import com.nckim.mvvmaacstudy.db.Contact
 import kotlinx.android.synthetic.main.contact_item.view.*
 
@@ -14,7 +16,13 @@ class ContactAdapter (private val itemClick: (Contact) -> Unit,
     private var contacts : List<Contact> = listOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
-        return ViewHolder(view)
+        val binding : ContactItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.contact_item,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -25,11 +33,9 @@ class ContactAdapter (private val itemClick: (Contact) -> Unit,
         return contacts.size
     }
 
-    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(private val binding : ContactItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(contact: Contact){
-            itemView.tv_name.text = contact.name
-            itemView.tv_number.text = contact.number
-            itemView.tv_initial.text = contact.initial.toString()
+            binding.contact = contact
             itemView.setOnClickListener { itemClick(contact) }
             itemView.setOnLongClickListener {
                 itemLongClick(contact)
